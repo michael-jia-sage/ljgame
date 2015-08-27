237b82ef9dac210c311d5ed7b6b65e6946200927
Zonggao Jia <jiazonggao@zonggao-jias-computer.local>
2015-08-26 23:36:11 -0700

    begin

--- ljgame/GameScene.m
+++ ljgame/GameScene.m
@@ -8,38 +8,53 @@
 
 #import "GameScene.h"
 
+int gameStatus = 0;
+
 @implementation GameScene
 
 -(void)didMoveToView:(SKView *)view {
-    /* Setup your scene here */
-    SKLabelNode *myLabel = [SKLabelNode labelNodeWithFontNamed:@"Chalkduster"];
-    
-    myLabel.text = @"Hello, World!";
-    myLabel.fontSize = 65;
-    myLabel.position = CGPointMake(CGRectGetMidX(self.frame),
-                                   CGRectGetMidY(self.frame));
-    
-    [self addChild:myLabel];
+    [self resetView];
 }
 
 -(void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event {
     /* Called when a touch begins */
-    
-    for (UITouch *touch in touches) {
-        CGPoint location = [touch locationInNode:self];
-        
-        SKSpriteNode *sprite = [SKSpriteNode spriteNodeWithImageNamed:@"Spaceship"];
-        
-        sprite.xScale = 0.5;
-        sprite.yScale = 0.5;
-        sprite.position = location;
-        
-        SKAction *action = [SKAction rotateByAngle:M_PI duration:1];
-        
-        [sprite runAction:[SKAction repeatActionForever:action]];
-        
-        [self addChild:sprite];
+    if (gameStatus == 0) {
+        gameStatus = 1;
+        // remove label
+        [[self childNodeWithName: @"myLabel"] removeFromParent];
+    } else {
+        gameStatus = 0;
+        // add label
+        [self resetView];
     }
+    
+//    for (UITouch *touch in touches) {
+//        CGPoint location = [touch locationInNode:self];
+//        
+//        SKSpriteNode *sprite = [SKSpriteNode spriteNodeWithImageNamed:@"Spaceship"];
+//        
+//        sprite.xScale = 0.5;
+//        sprite.yScale = 0.5;
+//        sprite.position = location;
+//        
+//        SKAction *action = [SKAction rotateByAngle:M_PI duration:1];
+//        
+//        [sprite runAction:[SKAction repeatActionForever:action]];
+//        
+//        [self addChild:sprite];
+//    }
+}
+
+-(void)resetView {
+    /* Setup your scene here */
+    SKLabelNode *myLabel = [SKLabelNode labelNodeWithFontNamed:@"Chalkduster"];
+    
+    myLabel.text = @"Tap to begin ...";
+    myLabel.fontSize = 65;
+    myLabel.position = CGPointMake(CGRectGetMidX(self.frame),
+                                   CGRectGetMidY(self.frame));
+    [myLabel setName:@"myLabel"];
+    [self addChild:myLabel];
 }
 
 -(void)update:(CFTimeInterval)currentTime {
