b964f608c762fa266878a0d9366605050be2b138
Zonggao Jia <jiazonggao@zonggao-jias-computer.local>
2015-08-26 21:40:29 -0700

    Initial Commit

---
+++ ljgame/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ljgame
+//
+//  Created by Zonggao Jia on 2015-08-26.
+//  Copyright (c) 2015 Zonggao Jia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ljgame/GameScene.h
@@ -0,0 +1,13 @@
+//
+//  GameScene.h
+//  ljgame
+//
+
+//  Copyright (c) 2015 Zonggao Jia. All rights reserved.
+//
+
+#import <SpriteKit/SpriteKit.h>
+
+@interface GameScene : SKScene
+
+@end
